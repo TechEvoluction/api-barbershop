6407541fd324d1372fd5814c22b4ce886053d9e2
Leonardo Silva <leonardoferreira032@gmail.com>
2026-09-21 11:53:30 -0300

    conflict

---
+++ src/Barbershop.Api/Endpoints/ScheduleEndpoints.cs
@@ -0,0 +1,19 @@
+﻿using Barbershop.Api.Extensions;
+using Barbershop.Shareable.Request;
+using MediatR;
+
+namespace Barbershop.Api.Endpoints;
+
+internal static class ScheduleEndpoints
+{
+    public static void MapScheduleEndpoint(this IEndpointRouteBuilder app)
+    {
+        var api = app.MapGroup("schedules")
+            .WithTags("Schedules");
+
+        api.MapGet("available-times", async (IMediator mediator) => await mediator.SendCommand(new SchedulesAvailableTimesRequest()))
+            .Produces(StatusCodes.Status200OK)
+            .WithSummary("Retorna os horários disponíveis de cada barbeiro para agendamento")
+            .RequireAuthorization();
+    }
+}
---
+++ src/Barbershop.Domain/Handler/SchedulesHandler.cs
@@ -0,0 +1,14 @@
+﻿using Barbershop.Shareable.Request;
+using MediatR;
+using OperationResult;
+
+namespace Barbershop.Domain.Handler;
+
+public class SchedulesHandler
+    : IRequestHandler<SchedulesAvailableTimesRequest, Result>
+{
+    public Task<Result> Handle(SchedulesAvailableTimesRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
